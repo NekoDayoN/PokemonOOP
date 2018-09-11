45aea707d1398320919f9fcaeff174de1dae606d
NekoDayoN <contato.santosgabriel@gmail.com>
2018-09-10 23:58:39 -0300

    Adding Caterpie info

--- src/pokemons/Caterpie.js
+++ src/pokemons/Caterpie.js
@@ -4,6 +4,13 @@ const Pokemon = require('./Pokemon');
 function Caterpie(nickname) {
     Pokemon.call(this, nickname || "Caterpie");
     this.types = [types.bug];
+    this.level = 1;
+    this.hp = 45;
+    this.attack = 30;
+    this.defense = 35;
+    this.spAttack = 20;
+    this.spDefense = 20;
+    this.speed = 45;
 }
 
 Caterpie.prototype = Object.create(Pokemon.prototype);
