f37b8adddff17f9af454ce02e011662acd6009dd
NekoDayoN <contato.santosgabriel@gmail.com>
2018-10-01 21:53:10 -0300

    Some more code refactoring

--- src/util/multiFileLoader.js
+++ src/util/multiFileLoader.js
@@ -1,5 +1,3 @@
-const _fetchFile = (path, name) => require(path + name);
-
 /**
  * Loads Multiple files from the same folder and returns it as an object
  */
@@ -7,7 +5,7 @@ const multiFileLoader = (path, files) => {
     const filesToExport = {};
 
     for(const file of files) {
-        let buffer = _fetchFile(path, file);
+        let buffer = require(path + file);
         filesToExport[file] = buffer;
     }
 
